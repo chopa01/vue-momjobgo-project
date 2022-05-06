2f748c32e44ef8621ff73f293ae2a56937de4126
chopa01 <101918488+chopa01@users.noreply.github.com>
2022-05-06 15:35:05 +0900

    배포main 수정

--- src/views/layout/MainView.vue
+++ src/views/layout/MainView.vue
@@ -1,23 +1,8 @@
-<template>  
+<template>
   <v-main>
-      <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="text-h6">todolist 기능</v-list-item-title>
-        <v-list-item-subtitle>
-          <div>
-        1. 작업할 일을 등록하세요 => 글 등록 가능 <p/>
-        2.체크박스 클릭시 <br/>
-        1) Complete로 인정 <br/>
-        2) 휴지통 아이콘 클릭 시 삭제<p/>
-        3. 소트 기능 및 건 수 보여주기<br/>
-        1) All - 전체<br/>
-        2) Active - 작업할 내용<br/>
-        3) Complete -  완료된 내용 <br/>
-        4) Clear completed - 체크박스 일괄 삭제 <br/>
-      </div>
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
   </v-main>
 </template>
 
