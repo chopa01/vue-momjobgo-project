79249c81a5eb4c28e86d7002d3034d4d47aee7e0
chopa01 <101918488+chopa01@users.noreply.github.com>
2022-05-12 09:26:47 +0900

    카카오지도

--- src/views/AxiosView.vue
+++ src/views/AxiosView.vue
@@ -11,10 +11,7 @@
 
          <!-- <v-text-field   v-model="keyword" 
             placeholder="검색어를 입력하세요"
-            solo  @keydown.enter="searchPlace"> </v-text-field>
-          <v-btn  elevation="2" @click="searchPlace">검색</v-btn> -->
-   
- 
+            solo  @keydown.enter="searchPlace"> </v-text-field>-->
 
   <div id="map" class="map"></div>  
  </v-container>
@@ -27,7 +24,6 @@ import axios from "axios";
 export default {
     data: () => ({
     keyword:'',
-
     }),
    mounted() {
          
@@ -45,9 +41,10 @@ export default {
    
     }
 },
+ 
 methods: {
     initMap() {
-        var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+       // var infowindow = new kakao.maps.InfoWindow({zIndex:1});
         var mapContainer = document.getElementById('map'), // 지도를 표시할 div
             mapOption = {
             center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
@@ -58,61 +55,59 @@ methods: {
         this.map= map;
             // console.log(map)
     },
+
     searchPlace(){  
-
-
-        var infowindow = new kakao.maps.InfoWindow({zIndex:1});     
+     
         const ps = new window.kakao.maps.services.Places();
 
-        ps.keywordSearch("분당", (data, status, pagination) => {
+        ps.keywordSearch(this.keyword, (data, status, pagination) => {
            if (status === kakao.maps.services.Status.OK) {
 
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
+        // LatLngBounds 객체에 좌표를 추가합니다npm
         
             var bounds = new kakao.maps.LatLngBounds();
+
             for (var i=0; i<data.length; i++) {
+                  
+                var iwContent = '<div style="padding:5px;z-index:-1;">'+ data[i].place_name +'</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                this.disPlayMarker(data[i].y, data[i].x,this.map,iwContent );
             
-                  var  marker = new kakao.maps.Marker({
-                    //map: this.map,
-                    position: new kakao.maps.LatLng(data[i].y, data[i].x) ,
-                    clickable: true  
-                });
-              
-            marker.setMap(this.map);
-          
-            var iwContent = '<div style="padding:5px;z-index:-1;">'+ data[i].place_name +'</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-               // iwPosition = new kakao.maps.LatLng(data[i].y, data[i].x), //인포윈도우 표시 위치입니다
-                iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-            // 인포윈도우를 생성하고 지도에 표시합니다
-            var infowindow = new kakao.maps.InfoWindow({
-             //   map: this.map, // 인포윈도우가 표시될 지도
-               //  position : iwPosition, 
-                content : iwContent,
-                removable : iwRemoveable
-            });
-infowindow.open(this.map, marker);
-               // 마커에 클릭이벤트를 등록합니다
-            kakao.maps.event.addListener(marker, 'click', function() {
-                // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다  
-    
-                infowindow.open(this.map, marker);
-            })
-
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-           
-
-            
             }       
-
-
             // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
             this.map.setBounds(bounds);
             } 
         });
         },
- 
+
+        disPlayMarker(xpos, ypos, map,iwContent) {
+          var  marker = new kakao.maps.Marker({
+                  position: new kakao.maps.LatLng(xpos, ypos) 
+              });          
+          marker.setMap(map);
+          this.displayInfowindow(marker, iwContent,this.map);
+        },
+
+        displayInfowindow (marker, iwContent,map) {
+          // 인포윈도우를 생성합니다
+           var infowindow = new kakao.maps.InfoWindow({
+                content : iwContent,
+          
+            });
+            // 마커에 마우스오버 이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'mouseover', function() {
+              // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+                infowindow.open(map,marker);
+            });
+
+            // 마커에 마우스아웃 이벤트를 등록합니다
+            kakao.maps.event.addListener(marker, 'mouseout', function() {
+                // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+                infowindow.close();
+            })
+
+        }
         }
  
 }
