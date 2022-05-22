86c8818f4bce17e37f4cfe50e3654d0af6a1c5c8
chopa01 <101918488+chopa01@users.noreply.github.com>
2022-05-22 11:10:24 +0900

    세션적용

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import page from './modules/page'
 import user from './modules/user'
-
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -17,5 +19,13 @@ export default new Vuex.Store({
   modules: {
     page,
     user
-  }
+  },
+  plugins : [createPersistedState({
+    paths : ["user"],
+    storage : {
+      getItem: (key) => ls.get(key),
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: (key) => ls.remove(key),
+    }
+  })]
 })
